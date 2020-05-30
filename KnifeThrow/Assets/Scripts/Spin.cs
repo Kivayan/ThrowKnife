63333782935fb18c6b913836051c400cc796dc48
Kivayan <kivayan@gmail.com>
2020-05-30 15:45:41 +0200

    Spin mode selector for Target

--- KnifeThrow/Assets/Scripts/Spin.cs
+++ KnifeThrow/Assets/Scripts/Spin.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    [SerializeField] float flatRotationSpeed = 50f;
+    [Header ("Simple Rotation")]
+    [SerializeField] bool simpleRotation = false;
+    [SerializeField] float simpleRotationSpeed = 60f;
 
+
+    [Header ("Sinusoidal Rotation")]
+    [SerializeField] bool sinusoidalRotation = false;
     [SerializeField] float sinBasSpeed = 50f;
     [SerializeField] float sinFrequency = 4f;
     [SerializeField] float sinMagintude = 40f;
@@ -21,15 +26,15 @@ public class Spin : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        SpinSinusoid();
+        RotationModeSelector();
     }
 
     void SpinBasic()
     {
-        transform.Rotate(Vector3.forward * flatRotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * simpleRotationSpeed * Time.deltaTime);
     }
 
     void SpinSinusoid()
@@ -38,4 +43,30 @@ public class Spin : MonoBehaviour
         sinSpeed = sinBasSpeed + Mathf.Sin(sinFrequency * sinTime) * sinMagintude;
         transform.Rotate(Vector3.forward * Time.deltaTime * sinSpeed);
     }
+
+    void RotationModeSelector()
+    {
+        if(
+            (simpleRotation && sinusoidalRotation ) 
+            ||
+            (!simpleRotation && !sinusoidalRotation ) 
+        )
+        {
+            throw new System.Exception ("Select only one of Rotation Modes in " + gameObject.name);
+        }
+
+        if(simpleRotation)
+        {
+            SpinBasic();
+        }
+    
+        if(sinusoidalRotation)
+        {
+            SpinSinusoid();
+        }
+    }
+    
+
+
+
 }
